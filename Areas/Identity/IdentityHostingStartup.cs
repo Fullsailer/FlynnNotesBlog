c5a3037b5ae42a27f64abf6fb20c6ffd40684cc9
John Flynn <40461634+Fullsailer@users.noreply.github.com>
2021-08-30 17:04:49 -0400

    Scaffolded Identity Pages
    Added Identity Pages in Areas for user Identity pages.

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using FlynnNotesBlog.Data;
+using FlynnNotesBlog.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(FlynnNotesBlog.Areas.Identity.IdentityHostingStartup))]
+namespace FlynnNotesBlog.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
