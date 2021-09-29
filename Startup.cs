dac729cc282afb3ea0b209d9f5f0079b606070d7
John Flynn <40461634+Fullsailer@users.noreply.github.com>
2021-09-29 12:49:05 -0400

    Added DataUtility and linked it to startup.cs
    Added DataUtility and linked it to startup.cs to connect to Heroku hosting and database.

--- Startup.cs
+++ Startup.cs
@@ -30,7 +30,7 @@ namespace FlynnNotesBlog
         {
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseNpgsql(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                    DataUtility.GetConnectionString(Configuration)));
             services.AddDatabaseDeveloperPageExceptionFilter();
 
             //services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
