2718e5750a10534bdb157373f000c2a2ccce99b6
John Flynn <40461634+Fullsailer@users.noreply.github.com>
2021-10-01 14:38:02 -0400

    Setting Up Email Service
    Added mail setting to appsetting.json added IBlogEmail Sender to Regiser
    Created Email Service Class
    Created IBlogEmailSender Interface
    Added EmailService and IBlogEmailSender to startup
    Added MailSetting View Model

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -14,6 +14,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
+using FlynnNotesBlog.Services;
 
 namespace FlynnNotesBlog.Areas.Identity.Pages.Account
 {
@@ -23,7 +24,8 @@ namespace FlynnNotesBlog.Areas.Identity.Pages.Account
         private readonly SignInManager<BlogUser> _signInManager;
         private readonly UserManager<BlogUser> _userManager;
         private readonly ILogger<RegisterModel> _logger;
-        private readonly IEmailSender _emailSender;
+        //private readonly IEmailSender _emailSender;
+        private readonly IBlogEmailSender _emailSender;
 
         public RegisterModel(
             UserManager<BlogUser> userManager,
