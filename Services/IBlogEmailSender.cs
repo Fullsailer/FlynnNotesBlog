5163ab91d5f609dbbea0481751c636c9071296b0
John Flynn <40461634+Fullsailer@users.noreply.github.com>
2021-10-15 16:04:51 -0400

    Updated Email Service
    Updated registration page, updated email confirmation page, added MailKit and MimeKit Nuget packages, added Task to build and send email to email service, made IBlogEmailSender more accesable by making it public.

--- Services/IBlogEmailSender.cs
+++ Services/IBlogEmailSender.cs
@@ -1,13 +1,17 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using MimeKit;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Threading.Tasks;
 
 namespace FlynnNotesBlog.Services
 {
-    interface IBlogEmailSender : IEmailSender
+    public interface IBlogEmailSender : IEmailSender
     {
         Task SendContactEmailAsync(string emailFrom, string name, string subject, string htmlMessage);
+
+        
     }
 }
