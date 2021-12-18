e363f2bed0bac43572e59d77dcdcaaa9902a50d0
John Flynn <40461634+Fullsailer@users.noreply.github.com>
2021-12-18 14:32:41 -0500

    Tag, Validation, Search, Sweet Alert
    Added ViewData Tag Values to Post Create View scripts.
    Added cdn content delivery network for sweetalerts2 to shared layout view.
    Added OOPS image to img root folder for sweetalert.
    Added custom CSS border for sweetalert popup, remmeber to open in private window to view or clear browers cache to view CSS changes.
     CustomJS:
    Added blank and duplicate tag entry sweetalert.
    Added Choose tag delete sweetalert.
    Added constant button and image popup for sweetalert.

--- wwwroot/js/Custom.js
+++ wwwroot/js/Custom.js
@@ -4,9 +4,19 @@ function AddTag() {
     //Get a reference to the TagEntry input element
     var tagEntry = document.getElementById("TagEntry");
 
-    //Create a new Select Option
-    let newOption = new Option(tagEntry.value, tagEntry.value); 
-    document.getElementById("TagList").options[index++] = newOption;
+    // Search function to detect and error state
+    let searchResult = search(tagEntry.value);
+    if (searchResult != null) {
+        //Trigger sweet alert for condition  in searchResult var
+        swalWithDarkButton.fire({
+            html: `<span class='font-weight-bolder'>${searchResult.toUpperCase()}</span>`
+        });
+    }
+    else {
+        //Create a new Select Option
+        let newOption = new Option(tagEntry.value, tagEntry.value);
+        document.getElementById("TagList").options[index++] = newOption;
+    }
 
     //Clear out the TagEntry control
     tagEntry.value = "";
@@ -15,11 +25,19 @@ function AddTag() {
 
 function DeleteTag() {
     let tagCount = 1;
+    let tagList = document.getElementById("TagList");
+    if (!tagList) return false;
+
+    if (tagList.selectedIndex == -1) {
+        swalWithDarkButton.fire({
+            html: "<span class='font-weight-bolder'>CHOOSE A TAG BEFORE DELETING</span>"
+        });
+        return true;
+    }
+
     while (tagCount > 0) {
-        let tagList = document.getElementById("TagList");
-        let selectedIndex = tagList.selectedIndex;
-        if (selectedIndex >= 0) {
-            tagList.options[selectedIndex] = null;
+        if (tagList.selectedIndex >= 0) {
+            tagList.options[tagList.selectedIndex] = null;
             --tagCount;
         }
         else
@@ -46,3 +64,30 @@ function ReplaceTag(tag, index) {
     let newOption = new Option(tag, tag);
     document.getElementById("TagList").options[index] = newOption;
 }
+
+// The search function will detect either an empty or duplicate tag.
+// and return an error string if an error is detected
+function search(str) {
+    if (str == "") {
+        return 'Empty tags are not permitted';
+    }
+
+    var tagsEl = document.getElementById('TagList');
+
+    if (tagsEl) {
+        let options = tagsEl.options;
+        for (let index = 0; index < options.length; index++) {
+            if (options[index].value == str)
+                return `The Tag #${str} was detected as a duplicate and not permitted`
+        }
+    }
+}
+
+const swalWithDarkButton = Swal.mixin({
+    customClass: {
+        confirmButton: 'btn btn-danger btn-sm btn-block btn-outline-dark'
+    },
+    imageUrl: '/assets/img/oops.jpg',
+    timer: 3000,
+    buttonsStyling: false
+});
